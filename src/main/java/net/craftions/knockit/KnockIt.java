537d2bba4b0dd1f18534fd8a7864bc0784a1c89a
MCTzOCK <dev@mctzock.de>
2021-05-27 19:17:13 +0200

    Added config api

--- src/main/java/net/craftions/knockit/KnockIt.java
+++ src/main/java/net/craftions/knockit/KnockIt.java
@@ -1,5 +1,10 @@
 package net.craftions.knockit;
 
+import net.craftions.knockit.commands.CommandSetSpawn;
+import net.craftions.knockit.events.EventBlockPlace;
+import net.craftions.knockit.events.EventPlayerDeath;
+import net.craftions.knockit.events.EventPlayerJoin;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KnockIt extends JavaPlugin {
@@ -9,7 +14,14 @@ public final class KnockIt extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println(prefix + "Loading KnockIt v" + this.getDescription().getVersion() + "...");
-
+        // register commands
+        getCommand("setspawn").setExecutor(new CommandSetSpawn());
+        // register events
+        Bukkit.getPluginManager().registerEvents(new EventPlayerJoin(), this);
+        Bukkit.getPluginManager().registerEvents(new EventPlayerDeath(), this);
+        Bukkit.getPluginManager().registerEvents(new EventBlockPlace(), this);
+        // finish
+        System.out.println(prefix + "Successfully loaded KnockIt!");
     }
 
     @Override
---
+++ src/main/java/net/craftions/knockit/events/EventBlockPlace.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 Ben Siebert. All rights reserved.
+ */
+package net.craftions.knockit.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class EventBlockPlace implements Listener {
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e){
+
+    }
+}
