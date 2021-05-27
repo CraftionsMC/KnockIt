4069272c486e01fe672f5154e0df2fb37753247a
MCTzOCK <dev@mctzock.de>
2021-05-27 19:19:45 +0200

    Added configuration preparing

--- src/main/java/net/craftions/knockit/KnockIt.java
+++ src/main/java/net/craftions/knockit/KnockIt.java
@@ -1,19 +1,38 @@
 package net.craftions.knockit;
 
 import net.craftions.knockit.commands.CommandSetSpawn;
+import net.craftions.knockit.config.Config;
 import net.craftions.knockit.events.EventBlockPlace;
 import net.craftions.knockit.events.EventPlayerDeath;
 import net.craftions.knockit.events.EventPlayerJoin;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+
 public final class KnockIt extends JavaPlugin {
 
     public static String prefix = "§7[§cKnock§9It§7] §r";
+    public static Config config = null;
 
     @Override
     public void onEnable() {
         System.out.println(prefix + "Loading KnockIt v" + this.getDescription().getVersion() + "...");
+        // prepare configuration
+        File rootDir = new File("plugins/KnockIt");
+        if (!rootDir.isDirectory()) {
+            rootDir.mkdirs();
+        }
+        File configFile = new File("plugins/KnockIt/config.yml");
+        if(!configFile.exists()){
+            try {
+                configFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        config = new Config(configFile, "conf");
         // register commands
         getCommand("setspawn").setExecutor(new CommandSetSpawn());
         // register events
