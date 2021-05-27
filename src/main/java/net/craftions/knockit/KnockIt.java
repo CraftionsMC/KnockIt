e1842f9d5e925b5a6db61fcd4710b7f32aebab6b
MCTzOCK <dev@mctzock.de>
2021-05-27 19:12:26 +0200

    Added some more files

--- src/main/java/net/craftions/knockit/KnockIt.java
+++ src/main/java/net/craftions/knockit/KnockIt.java
@@ -4,14 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KnockIt extends JavaPlugin {
 
+    public static String prefix = "§7[§cKnock§9It§7] §r";
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println(prefix + "Loading KnockIt v" + this.getDescription().getVersion() + "...");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
---
+++ src/main/java/net/craftions/knockit/commands/CommandSetSpawn.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Ben Siebert. All rights reserved.
+ */
+package net.craftions.knockit.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandSetSpawn implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        return true;
+    }
+}
